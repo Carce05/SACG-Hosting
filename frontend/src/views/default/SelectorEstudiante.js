a8c631f5a79d596b135aa1b86167c23d3f213fe2
Erick <eguillen80064@ufide.ac.cr>
2023-02-07 12:31:43 -0600

    Select

--- frontend/src/views/default/SelectorEstudiante.js
+++ frontend/src/views/default/SelectorEstudiante.js
@@ -33,12 +33,11 @@ const SelectorEstudiante = (props) => {
       data.forEach((value) => {
         results.push({
           value: value.cedula,
-          label: value.nombre,
+          label: `${value.nombre} ${value.apellido} ( ${value.cedula} )`,
         });
       });
       // Update the options state
-      setOptions([
-        {value: 'Select a company', label: ''}, 
+      setOptions([ 
         ...results
       ])
     }
@@ -47,108 +46,12 @@ const SelectorEstudiante = (props) => {
     fetchData();
   }, []);
 
-
   const { label, name, ...rest } = props;
-
-  // console.log(options.nombre);
 
   const initialValues = { email: '' };
 
-    /*
-    const rawResponse = fetch(, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    });
-
-    const estudiante = JSON.parse(rawResponse[0]);
-    
-    console.log(estudiante);
-    // const nombre = estudiante.nombre;
-    */
-   
-    /*
-    const respuesta = async function ()  {
-      const response = await fetch(`http://localhost:8080/api/estudiantes/EstudiantesAsocidados/${usuario}`)
-  
-      const data = await response.json()
-  
-      // const respuesta = JSON.parse(data);
-      
-       const nombre = data[0].nombre.toString();
-
-      // console.log(data[0].nombre);
-      return nombre;
-  }
-
-  let name = await respuesta();
-  console.log(name);
-  */
-
-  // const resultado = llenar();
-  // console.log(resultado);
-
-  /*
-  useEffect(() => {
-    fetch(`http://localhost:8080/api/estudiantes/EstudiantesAsocidados/${usuario}`)
-       .then((response) => response.json())
-       .then((data) => {
-          console.log(this.data);
-          setValue(this.data);
-
-       })
-       .catch((err) => {
-          console.log(err.message);
-       });
- }, []);
- */
-
-
-/*
- useEffect(() => {
-  const fetchata = async () => {
-
-      const response = await fetch(
-        `http://localhost:8080/api/estudiantes/EstudiantesAsocidados/${usuario}`);
-         const data = await response.json();
-
-         // use only 3 sample data
-         setValue( data.slice( 0,1) )
-     
-  }
-
-  // Call the function
-  fetchata();
-}, []);
-*/
-
-
-/*
- function callApi() {
-  fetch(`http://localhost:8080/api/estudiantes/EstudiantesAsocidados/${usuario}`, { method: 'GET' })
-    .then(response => response.json())
-    .then(data => console.log(data[0].nombre) ) // Displays the firstName from the API response
-}
-*/
-
-
-
-
-  const estudiantes = [
-    { value: '1-1828-0064', label: '' },
-    { value: '1-1221-4354', label: 'Christopher Arce (1-1221-4354)' },
-  ];
-
-
-    
-  
-
-
-
   const formik = useFormik({ initialValues, validationSchema });
-  const { handleSubmit, values, touched, errors } = formik;
+  const { handleSubmit, option, touched, errors } = formik;
 
   const leftSide = (
     <div className="min-h-100 d-flex align-items-center">
@@ -191,7 +94,8 @@ const SelectorEstudiante = (props) => {
               <CsLineIcons icon="user" />
               <Select classNamePrefix="react-select" 
                     options={options} 
-                    value={Option} 
+                    value={option} 
+                    onChange={setOptions} 
                     placeholder="Seleccione" 
               />
               {errors.email && touched.email && <div className="d-block invalid-tooltip">{errors.email}</div>}
