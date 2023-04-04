f21726c8e9533ae8353fbdb71e7d9427889c384a
Erick <eguillen80064@ufide.ac.cr>
2023-04-04 17:04:30 -0600

    Modifico index back

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,1 @@
-/*const dbConnect= require('./server');
-
-dbConnect().then((resp)=>{
-resp.find({name:'nord'}).toArray().then((data)=>{
-console.log(data)
-})
-})
-
-const main=async ()=>{
-   let data = await dbConnect();
-   data = await data.find({name:'nord'}).toArray();
-   console.log(data)
-}
-
-main()
-*/
+// Esto es un API
